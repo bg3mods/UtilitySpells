03ff10f2339a74150ac9f6ec1e0e9ed4d97bd32b
CamilleHBP <11173924+CamilleHbp@users.noreply.github.com>
2023-09-02 19:08:58 +0200

    feat: add new lua table function

--- UtilitySpells/Mods/UtilitySpells/ScriptExtender/Lua/BootstrapServer.lua
+++ UtilitySpells/Mods/UtilitySpells/ScriptExtender/Lua/BootstrapServer.lua
@@ -4,15 +4,58 @@ Ext.Osiris.RegisterListener("ClearFlag", 3, "after", function(FLAG, GUIDSTRING, 
     end
 end)
 
+-- Custom ToggleTag functions
+local function UTSP_ToggleTag(character, tag_guid, tag_notification)
+    local character_string = "" .. character
+    local add_notification = "You have a new ability: " .. tag_notification
+    local remove_notification = "You have lost an ability: " .. tag_notification
+
+    if Osi.IsTagged(character, tag_guid) == 1 then
+        Osi.ClearTag(character, tag_guid)
+        Osi.ShowNotification(character_string, remove_notification)
+    else
+        Osi.SetTag(character, tag_guid)
+        Osi.ShowNotification(character_string, add_notification)
+    end
+end
+
+-- Alternative function with table
 Ext.Osiris.RegisterListener("CastSpell", 5, "after", function(char, spell, nil1, nil2, nil3)
-    local DETECTTHOUGHS = "7e0395f8-aa53-4549-9af3-e2d203d678ee"
-    if spell == "UTSP_Shout_ToggleDetectThoughts" then
-        if Osi.IsTagged(GetHostCharacter(), DETECTTHOUGHS) == 1 then
-            Osi.ClearTag(GetHostCharacter(), DETECTTHOUGHS)
-            ShowNotification(GetHostCharacter(), "You cannot detect thoughts in others anymore")
-        else
-            Osi.SetTag(GetHostCharacter(), DETECTTHOUGHS)
-            ShowNotification(GetHostCharacter(), "You can detect thoughts in others")
-        end
+    local character = GetHostCharacter();
+    local tagTable = {
+        ["UTSP_Shout_ToggleBackground_Acolyte"] = {tag_guid = "906d1961-e9c6-4170-b775-62797a0efe1c", tag_notification = "acolyte background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Charlatan"] = {tag_guid = "07925e5b-af3d-4743-a565-b9fee04c24ac", tag_notification = "charlatan background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Criminal"] = {tag_guid = "74ed9de3-fff5-419a-949b-18bd5e17944e", tag_notification = "criminal background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Entertainer"] = {tag_guid = "bd3cfefd-5fe2-47ea-bd3c-e99eaad9b714", tag_notification = "entertainer background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_FolkHero"] = {tag_guid = "9c38904c-4aed-4d22-9038-fc7d103aaed5", tag_notification = "folk hero background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_GuildArtisan"] = {tag_guid = "baf9fb0e-f535-40a0-bb78-3ec289d84e66", tag_notification = "guild artisan background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Hermit"] = {tag_guid = "2fa3f042-2b50-41b2-8045-39ddd4421ca9", tag_notification = "hermit background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Noble"] = {tag_guid = "56825486-9cfc-4ec2-87d1-c8065c41774b", tag_notification = "noble background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Outlander"] = {tag_guid = "d81df6f9-9f6e-4146-841a-9f285e6ee879", tag_notification = "outlander background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Sage"] = {tag_guid = "dad18490-c61d-4958-805c-f017eb68f8a0", tag_notification = "sage background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Soldier"] = {tag_guid = "d135f265-c2e1-4077-a836-b548ee871681", tag_notification = "soldier background tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleBackground_Urchin"] = {tag_guid = "2faee476-d77b-42a7-a4c6-2b9ac934673c", tag_notification = "urchin background tag", callback = UTSP_ToggleTag},
+
+        ["UTSP_Shout_ToggleClass_Barbarian"] = {tag_guid = "", tag_notification = "barbarian class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Bard"] = {tag_guid = "", tag_notification = "bard class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Druid"] = {tag_guid = "", tag_notification = "druid class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Fighter"] = {tag_guid = "1ae7017c-4884-4a43-bc4a-742fa0d201c0", tag_notification = "fighter class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Monk"] = {tag_guid = "", tag_notification = "monk class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Ranger"] = {tag_guid = "", tag_notification = "ranger class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Rogue"] = {tag_guid = "", tag_notification = "rogue class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Sorcerer"] = {tag_guid = "", tag_notification = "sorcerer class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Warlock"] = {tag_guid = "", tag_notification = "warlock class tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleClass_Wizard"] = {tag_guid = "", tag_notification = "wizard class tag", callback = UTSP_ToggleTag},
+
+        ["UTSP_Shout_ToggleDialogue_ComprehendLanguages"] = {tag_guid = "1d6e36a6-d757-449b-8040-2af3f1a99c3a", tag_notification = "comprehend languages tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleDialogue_DetectThoughts"] = {tag_guid = "7e0395f8-aa53-4549-9af3-e2d203d678ee", tag_notification = "detect thoughts tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleDialogue_ForbiddenKnowledge"] = {tag_guid = "673cb6af-f12b-4d6e-abfa-1bb83cf4ce44", tag_notification = "forbidden knowledge tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleDialogue_PetPal"] = {tag_guid = "dc860a81-f3c2-4c1a-ab90-e7583324845c", tag_notification = "pet pal tag", callback = UTSP_ToggleTag},
+        ["UTSP_Shout_ToggleReligion"] = {tag_guid = "", tag_notification = "", callback = ""}
+    }
+
+    if tagTable[spell] ~= nil then
+        Ext.Utils.Print('spell: ', spell)
+        tagTable[spell].callback(character, tagTable[spell].tag_guid, tagTable[spell].tag_notification)
     end
 end)
