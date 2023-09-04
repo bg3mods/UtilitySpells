201e40499e3b3df07e7583466cd2fa26b14857cf
CamilleHBP <11173924+CamilleHbp@users.noreply.github.com>
2023-09-04 23:18:36 +0200

    feat: working potion description

--- create-package.ps1
+++ create-package.ps1
@@ -5,14 +5,16 @@ if ($null -eq (Get-Command "divine.exe" -ErrorAction SilentlyContinue)) {
 
 $modPath = "C:/Dev/Baldur's Gate 3/Mods/UtilitySpells"
 $modName = "UtilitySpells"
+
 if ($PSBoundParameters.ContainsKey("p1")) {
-    Write-Host "Mod path provided. The package will be sourced from ""${p1}/${modName}"""
+    Write-Host "Mod path provided. The package will be sourced from ""${p1}/${modName}/${modName}"""
     $modPath = $p1
 }
 else {
     Write-Host "WARNING: no path provided. Using the default value as LSlib requires an absolute value."
 }
-Write-Host "Mod path provided. The package will be sourced from ""${modPath}/${modName}"""
+Write-Host "The package will be sourced from ""${modPath}/${modName}/${modName}"""
 
 Write-Host "Creating package from source files"
-divine.exe -a create-package -g bg3 -s "${modPath}/${modName}" -d "${modPath}/release/${modName}.pak"
+divine.exe -g bg3 -a convert-loca -s "${modPath}/${modName}/Localization/English/UtilitySpells.xml" -d "${modPath}/${modName}/Localization/English/UtilitySpells.loca"
+divine.exe -g bg3 -a create-package -s "${modPath}/${modName}" -d "${modPath}/release/${modName}.pak"
